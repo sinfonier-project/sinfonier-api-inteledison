905cbe858973d39c1856085fb263388d1b4f2847
root <pedrojesus.martinez@global.11paths.com>
2015-11-11 10:24:16 +0000

    Add headers

--- plugins/screen_control.py
+++ plugins/screen_control.py
@@ -1,3 +1,14 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = ["Eva Suárez (@evasuarez22)", "Pedro J. Martínez (@pejema44)"]
+__copyright__ = "Copyright 2015, Sinfonier Project"
+__maintainer__ = ["Eva Suárez (@evasuarez22)", "Pedro J. Martínez(@pejema44)"]
+__email__ = ["eva@sinfonier-project.net","pedro@sinfonier-project.net"]
+__status__ = "Beta"
+__version__ = "0.1"
+
+
 import sys
 
 import socket
